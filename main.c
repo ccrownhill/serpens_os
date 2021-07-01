c8f427a8a7b58638dca16f9a251ec98e5d8bbd60
undefined-None <constantin.kronbichler@protonmail.com>
2021-07-01 20:03:40 +0200

    fixed typo

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ char* VGA_MEMORY = (char*) (0xb8000);
 
 /* Calculation of the addresses with this formula
  * destination_address = 0xb8000 + 2 * (row * 80 + col)
- * where row and col counting start at 0
+ * where row and col counting starts at 0
  * Rows: 10, 12, 14
  */
 char* FIELD_ADDRESSES[9] = {(char*) (0xb8686), (char*) (0xb868c), (char*) (0xb8692), (char*) (0xb87c6), (char*) (0xb87cc), (char*) (0xb87d2), (char*) (0xb8906), (char*) (0xb890c), (char*) (0xb8912)};
